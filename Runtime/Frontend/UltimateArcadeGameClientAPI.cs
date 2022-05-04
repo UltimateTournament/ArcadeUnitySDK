f20587e1411da6520e52e2274eb209d12328e81b
Martin Rauscher <martin@ultimatetournament.io>
2022-05-04 18:06:38 +0200

    initial commit

---
+++ Runtime/Frontend/UltimateArcadeGameClientAPI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UltimateArcade.Frontend
+{
+    public class UltimateArcadeGameClientAPI
+    {
+        public UltimateArcadeGameClientAPI()
+        {
+            // TODO, take in 
+            // * server base URL
+            // * auth token (signed JWT)
+            //   * game-id is derived from token 
+
+        }
+
+        public Task<Collection<Lobbies>> GetAvailableLobbies()
+        {
+        }
+    }
+
+    public class Lobby
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Runtime/Server/UltimateArcadeGameServerAPI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UltimateArcade.Server
+{
+    public class UltimateArcadeGameServerAPI
+    {
+
+        public UltimateArcadeGameServerAPI()
+        {
+        }
+
+        public Task Init()
+        {
+            // get token from environment or pod annotation etc
+        }
+
+        public Task<PlayerInfo> ActivateUser(string playerToken)
+        {
+            // player token is signed JWT containing
+            // * slipID
+            // * userID
+            // * server info (game, serverid)
+        }
+    }
+
+    public class PlayerInfo
+    {
+        public string Name { get; set; }
+        public string PlayerID { get; set; }
+        public string SlipID { get; set; }
+        public int TokensInGame { get; set; }
+    }
+
+}
