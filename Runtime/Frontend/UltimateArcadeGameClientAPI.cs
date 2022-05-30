f9f09f896d52ae9c07c91433c227edb1d69ed1da
Martin Rauscher <martin@ultimatetournament.io>
2022-05-30 10:02:19 +0200

    remove obsolete calls

--- Runtime/Frontend/UltimateArcadeGameClientAPI.cs
+++ Runtime/Frontend/UltimateArcadeGameClientAPI.cs
@@ -16,15 +16,6 @@ namespace UltimateArcade.Frontend
         public Task<UserInfo> GetUserInfo()
         {
         }
-
-        public Task<Match> MatchmakeLeaderboard(string leaderboardID)
-        {
-        }
-
-        // returns some old (closed), some open and potentially some future leaderboards
-        public Task<List<Leaderboard>> GetLeaderboards()
-        {
-        }
     }
 
     // will be thrown when trying to call any client methods while the token is expired.
@@ -36,34 +27,5 @@ namespace UltimateArcade.Frontend
         public string Name { get; set; }
         public string Tokens { get; set; }
         //TODO game-sepcific data like skin etc
-    }
-
-    public class Match
-    {
-        // opaque token you need to pass to the game server
-        public string GameToken { get; set; }
-        // websocket address to connect to e.g. wss://my-server.ua.io:7123/ws
-        public string ServerAddress { get; set; }
-    }
-
-    public class Leaderboard
-    {
-        public string ID { get; set; }
-        public string Name { get; set; }
-        public int EntryPrice { get; set; }
-        public int TokensToWin { get; set; }
-        public DateTime OpenAt { get; set; }
-        public DateTime CloseAt { get; set; }
-        public List<Entry> Board { get; set; }
-    }
-
-    public class Entry
-    {
-        public string PlayerName { get; set; }
-        // could be the same for multiple players
-        public int Rank { get; set; }
-        public int Score { get; set; }
-        // what they got if the leaderboard is closed
-        public int? TokensWon { get; set; }
     }
 }
