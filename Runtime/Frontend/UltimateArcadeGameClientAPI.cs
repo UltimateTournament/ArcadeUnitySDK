5f89b430b8b23c83ff9096ab2dcc4a71dbf4e8bd
Martin Rauscher <hades32@gmail.com>
2022-05-31 23:59:51 +0200

    implement frontend code

--- Runtime/Frontend/UltimateArcadeGameClientAPI.cs
+++ Runtime/Frontend/UltimateArcadeGameClientAPI.cs
@@ -1,29 +1,62 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace UltimateArcade.Frontend
 {
     public class UltimateArcadeGameClientAPI
     {
+        private readonly string gameToken;
+        private readonly string baseServerName;
+
         // baseServerName would be "staging.ultimatearcade.io" or "ultimatearcade.io"
         // userToken is game specific and depending on hosting method can be provided via different channels
         public UltimateArcadeGameClientAPI(string gameToken, string baseServerName)
         {
+            this.gameToken = gameToken;
+            this.baseServerName = baseServerName;
         }
 
-        public Task<GameInfo> GetGameInfo()
+        public GameInfo GetGameInfo()
         {
+            string tokenJson = System.Text.Encoding.Default.GetString(Convert.FromBase64String(this.gameToken.Split('.')[1]));
+            var address = JsonUtility.FromJson<serverInfo>(tokenJson).Address;
+            return new GameInfo { ServerAddress = address };
         }
 
-        public Task<UserInfo> GetUserInfo()
+        public IEnumerator GetUserInfo(Action<UserInfo> callback, Action<string> errorCallback)
         {
+            using (var webReq = new UnityWebRequest("https://userapi." + this.baseServerName + "/games/player-profile"))
+            {
+                webReq.SetRequestHeader("Authorization", "Bearer " + this.gameToken);
+                yield return webReq.SendWebRequest();
+
+                switch (webReq.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        errorCallback("Error: " + webReq.error);
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        errorCallback("HTTP Error: " + webReq.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        callback(JsonUtility.FromJson<UserInfo>(webReq.downloadHandler.text));
+                        break;
+                }
+            }
         }
 
-        public Task CloseGame()
+        public void CloseGame()
         {
         }
+    }
+
+    class serverInfo
+    {
+        [Newtonsoft.Json.JsonProperty(PropertyName = "addr")]
+        public string Address { get; set; }
     }
 
     // will be thrown when trying to call any client methods while the token is expired.
