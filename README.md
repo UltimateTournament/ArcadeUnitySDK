6ba1bb7396431a4852b5ffdb802affb61b905a2e
Martin Rauscher <hades32@gmail.com>
2022-06-01 18:57:40 +0200

    implement server side

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Ultimate Arcade Unity Game SDK
 
-## About
-
 This is the Unity SDK for integrating games (servers and frontends) into the Ultimate Arcade.
 
 The following diagram shows how the SDK is expected to be used, both on the client side (blue) and on the server side (yellow).
@@ -14,11 +12,13 @@ other parts of the system, what actions can be performed.
 * The **Match Token** gets passed from the Arcade Launcher to the game's website where it has to be passed into the SDK and grants permission to play exactly once on a specific server
 * The **Server Token** is handled transparently by the SDK and is just listed for completeness. It ensure server and game token match up. 
 
+
 ## Using it
 
 ### Server Side
 
 * Ensure you're listening with TCP (WebSockets) on the port that is set in the `PORT` environment variable
+
 
 ## Installation
 
@@ -33,3 +33,11 @@ other parts of the system, what actions can be performed.
 
 If you installed the SDK using the Git URL you can simply open the package manager and re-paste the Git URL.
 This should force a download of the latest code.
+
+
+## Developing
+
+When adding/updating protobuf files, remember to 
+* keep the C#-specific properties (namespace)
+* fix relative paths to match this repo
+* update the .csproj file for additions/deletions
--- Runtime/Server/UltimateArcadeGameServerAPI.cs
+++ Runtime/Server/UltimateArcadeGameServerAPI.cs
@@ -1,54 +1,134 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using Agones;
+using ArcadeSlipManager;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Configuration;
+using Newtonsoft.Json;
 
 namespace UltimateArcade.Server
 {
     public class UltimateArcadeGameServerAPI
     {
+        private AgonesSDK agones;
+        private string? serverToken;
+        private string? externalAddress;
+        private SlipManager.SlipManagerClient smClient;
 
         public UltimateArcadeGameServerAPI()
         {
+            this.agones = new AgonesSDK();
+            var slipManagerAddr = System.Environment.GetEnvironmentVariable("SLIP_MANAGER_ADDR");
+            if (slipManagerAddr == null)
+            {
+                slipManagerAddr = "slip-manager.slip-manager.svc.cluster.local:9090";
+            }
+            // if we switch to .NET core we need this
+            // AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var defaultMethodConfig = new MethodConfig
+            {
+                RetryPolicy = new RetryPolicy
+                {
+                    MaxAttempts = 3,
+                    InitialBackoff = TimeSpan.FromSeconds(0.1),
+                    MaxBackoff = TimeSpan.FromSeconds(1),
+                    BackoffMultiplier = 2,
+                    RetryableStatusCodes = { StatusCode.Unavailable }
+                }
+            };
+            ServiceConfig svcConfig = new ServiceConfig();
+            svcConfig.MethodConfigs.Add(defaultMethodConfig);
+            var channel = GrpcChannel.ForAddress("http://" + slipManagerAddr, new GrpcChannelOptions { ServiceConfig = svcConfig });
+            this.smClient = new SlipManager.SlipManagerClient(channel);
         }
+
+        private static readonly string[] neededAnnotations = new[] { "server-token", "random-seed", "external-address" };
 
         public Task<ServerData> Init()
         {
-            // get server token from environment or pod annotation etc and store it in instance
+            var wait = new AutoResetEvent(false);
+            var randomSeed = "";
+            this.agones.WatchGameServer(gs =>
+            {
+                if (gs.ObjectMeta.Annotations.Keys.Intersect(neededAnnotations).Count() == neededAnnotations.Length)
+                {
+                    this.serverToken = gs.ObjectMeta.Annotations["server-token"];
+                    this.externalAddress = gs.ObjectMeta.Annotations["external-address"];
+                    randomSeed = gs.ObjectMeta.Annotations["random-seed"];
+                    wait.Set();
+                }
+            });
+            return Task.Run(() =>
+            {
+                wait.WaitOne();
+                return new ServerData
+                {
+                    RandomSeed = randomSeed,
+                    ExternalAddress = this.externalAddress,
+                };
+            });
         }
 
         // you need to wait for this call to finish before exiting
-        public Task Shutdown()
+        public async Task<bool> Shutdown()
         {
+            var res = await this.agones.ShutDownAsync();
+            return res.StatusCode == Grpc.Core.StatusCode.OK;
         }
 
-        public Task<PlayerInfo> ActivateUser(string playerToken)
+        public async Task ActivateUser(string playerToken)
         {
-            // player token is signed JWT containing
-            // * slipID
-            // * userID
-            // * server info (game, serverid)
+            // request is empty because all necessary data is in the playerToken
+            await this.smClient.ActivateSlipAsync(new ActivateSlipReq(), headers: CreateAuthHeaders(playerToken));
+            return;
+        }
+
+        private Metadata CreateAuthHeaders(string playerToken)
+        {
+            var headers = new Metadata();
+            headers.Add("authorization", "bearer " + this.serverToken);
+            headers.Add("x-player-token", "bearer " + playerToken);
+            return headers;
         }
 
         // should be called for all players in leaderboard games
-        public Task FinishSessionWithScore(string playerToken, int score)
+        public async Task FinishSessionWithScore(string playerToken, int score)
         {
+            await this.smClient.ReportScoreForLeaderboardAsync(new ReportScoreForLeaderboardReq { Score = score }, headers: CreateAuthHeaders(playerToken));
         }
 
         // should be called when a user loses against another player in a non-leaderboard game
-        public Task FinishSessionWithLoss(string loserPlayerToken, string winnerPlayerToken)
+        public async Task FinishSessionWithLoss(string loserPlayerToken, string winnerPlayerToken)
         {
+            string tokenJson = System.Text.Encoding.Default.GetString(Convert.FromBase64String(winnerPlayerToken.Split('.')[1]));
+            var winnerToken = JsonConvert.DeserializeObject<playerToken>(tokenJson);
+            await this.smClient.SlipDefeatedAsync(new SlipDefeatedReq { WinningPlayerID = winnerToken.PlayerID, WinningPlayerSlipID = winnerToken.SlipID }, headers: CreateAuthHeaders(loserPlayerToken));
         }
 
         // should be called when a user leaves a non-leaderboard game without losing
-        public Task FinishSession(string playerToken)
+        public async Task FinishSession(string playerToken)
         {
+            // request is empty because all necessary data is in the playerToken
+            await this.smClient.SettleSlipAsync(new SettleSlipReq(), headers: CreateAuthHeaders(playerToken));
         }
 
         // report game-spefic metrics/events, like "player.joined", "player.found-secret", ...
-        public void ReportKPI(string eventID, Dictionary<string,object> data)
+        public void ReportKPI(string eventID, Dictionary<string, object> data)
         {
         }
+    }
+
+    class playerToken
+    {
+        [Newtonsoft.Json.JsonProperty(PropertyName = "pid")]
+        public string PlayerID { get; set; }
+
+        [Newtonsoft.Json.JsonProperty(PropertyName = "sid")]
+        public string SlipID { get; set; }
     }
 
     public class Debug
@@ -65,14 +145,8 @@ namespace UltimateArcade.Server
     public class ServerData
     {
         public string RandomSeed { get; set; }
+        public string ExternalAddress { get; set; }
     }
 
-    public class PlayerInfo
-    {
-        public string Name { get; set; }
-        public string PlayerID { get; set; }
-        public string SlipID { get; set; }
-        public int TokensInGame { get; set; }
-    }
 
 }
