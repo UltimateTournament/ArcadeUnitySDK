32acdd36829b1566d99e159e37cba660fa3302d0
Martin Rauscher <hades32@gmail.com>
2022-06-10 13:17:46 +0200

    expose random seed

--- Runtime/Frontend/AutoConnect.cs
+++ Runtime/Frontend/AutoConnect.cs
@@ -24,11 +24,26 @@ public class AutoConnect : MonoBehaviour
 
 #else
 
-    private UltimateArcadeGameServerAPI api;
+    private UltimateArcadeGameServerAPI api; 
+    public delegate void ServerReady();
+    public static event ServerReady OnServerReady;
+
+    /// <summary>
+    // use this seed to derive all randomness in your game to guarantee
+    // that all players have the same fair experience
+    /// </summary>
+    public static string RandomSeed { get; set; }
 
     void Start()
     {
         var nm = this.GetComponent<NetworkManager>();
+        var swt = this.GetComponent<SimpleWebTransport>();
+        var portStr = Environment.GetEnvironmentVariable("PORT");
+        if (portStr != null)
+        {
+            var port = int.Parse(portStr);
+            swt.port = (ushort)port;
+        }
         nm.StartServer();
         this.api = new UltimateArcadeGameServerAPI();
         StartCoroutine(this.init(0));
@@ -37,17 +52,16 @@ public class AutoConnect : MonoBehaviour
     private IEnumerator init(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        yield return api.Init(this.ServerReady, this.ServerNotReady);
+        yield return api.Init(this.onServerReady, this.onServerNotReady);
     }
 
-    private void ServerReady(ServerData obj)
+    private void onServerReady(ServerData obj)
     {
         UADebug.Log("random seed: "+obj.RandomSeed);
-        //TODO use this seed to derive all randomness in your game to guarantee
-        // that all players have the same fair experience
+        OnServerReady();
     }
 
-    private void ServerNotReady(string obj)
+    private void onServerNotReady(string obj)
     {
         StartCoroutine(this.init(1));
     }
